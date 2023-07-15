24d2a64ed727fa6dccafd1ae97a0b8f1a84d5513
Jaqobel <me.jakub.gil@gmail.com>
2023-07-15 23:42:08 +0200

    First ver of win condition

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -23,9 +23,57 @@ class Board
 
   end
 
+  def check_win()
+    #Check for horizontal win
+    for i in @board
+      if ((i.uniq.count <= 1) && (i[0] != nil))
+        return i[0]
+      end
+    end
+
+    #Check for vertical win
+    for i in @board.transpose()
+      if ((i.uniq.count <= 1) && (i[0] != nil))
+        return i[0]
+      end
+    end
+
+    #Check for diagonal win 1
+    i = 0
+    counter = 0
+    while i<@size
+      if ((@board[i][i] == @board[0][0]) && (@board[i][i] != nil))
+        counter = counter+1
+      end
+      i = i+1
+    end
+
+    if counter == @size
+      return @board[0][0]
+    end
+
+    #Check for diagonal win 2
+    i = 0
+    counter = 0
+    while i<@size
+      if ((@board[i][@size -1 - i] == @board[0][@size -1]) && (@board[i][@size -1 - i] != nil))
+        counter = counter+1
+      end
+      i = i+1
+    end
+
+    if counter == @size
+      return @board[0][@size-1]
+    end
+    #No win? Return 0
+    return 0
+  end
 end
 
 
 plansza = Board.new(3)
-plansza.board[0][0] = -1
+plansza.board[2][0] = -1
+plansza.board[1][1] = -1
+plansza.board[0][2] = -1
 plansza.print_board
+puts plansza.check_win
